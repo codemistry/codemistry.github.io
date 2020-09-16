35c03d50da4000a85895a92588a6725854b9387c
Hasan Misbah <hasanmisbah01@gmail.com>
2020-09-17 03:33:53 +0600

    Console Cleared

--- js/scripts.js
+++ js/scripts.js
@@ -227,7 +227,7 @@
             event.preventDefault();
 
             var $form = $(this);
-            $.post($form.attr("action"), $form.serializeArray()).then(function () {
+            $.post($form.attr("action"), $form.serialize()).then(function () {
                 cformSuccess();
             });
         }
